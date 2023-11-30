9ef0a5e8416f67e336918f06f564fcd4c28a7a1c
purnendu <75769105+Purnendu-tudu@users.noreply.github.com>
2023-12-01 01:47:14 +0530

    upper to lower lower to upper vice versa

---
+++ assignment_2/string_cap_to_s_and_vv/main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+int str_len(char *c){
+    int len = 0;
+    while(*c != '\0'){
+        len++;
+        c++;
+    }
+    return len;
+
+}
+
+int main(){
+    char input_str[100];
+    scanf("%[^\n]%*c", input_str);
+    int len = str_len(input_str);
+    for(int i = 0 ; i < len ; i++){
+        if( (int)input_str[i] < 123 && (int)input_str[i] > 96){
+            input_str[i] = (char)((int)input_str[i] - 32);
+        }else if( (int)input_str[i] < 91 && (int)input_str[i] > 64){
+            input_str[i] = (char)((int)input_str[i] + 32);
+        }
+    }
+
+    for(int j = 0; j <= len; j++){
+        printf("%c",input_str[j]);
+
+    }
+    return 0;
+}
